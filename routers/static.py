65b7898afd71f2618d2fe36577e3368a4d793fde
kuru-bana <kuru-api@hotmail.com>
2026-06-10 00:18:20 +0900

    Update static.py

--- routers/static.py
+++ routers/static.py
@@ -17,7 +17,7 @@ async def whats():
 
 @router.get("/version")
 async def version():
-    return {"ver": "1.32"}
+    return {"ver": "1.35"}
 
 
 # ── Link list status ──────────────────────────────────────────────────────────
